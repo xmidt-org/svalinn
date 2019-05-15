e2176edf56c3b355d43fc987c66c3b9f47d56dfb
Jack Murdock <kcajmagic@gmail.com>
2019-05-15 12:59:21 -0700

    improve svalinn code smell

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/Comcast/webpa-common/xmetrics"
+	"github.com/go-kit/kit/metrics/provider"
 	olog "log"
 	"net/http"
 	_ "net/http/pprof"
@@ -47,7 +49,6 @@ import (
 
 	gokithttp "github.com/go-kit/kit/transport/http"
 
-	"github.com/Comcast/webpa-common/bookkeeping"
 	"github.com/Comcast/webpa-common/logging/logginghttp"
 	"github.com/Comcast/webpa-common/server"
 	"github.com/Comcast/webpa-common/xhttp/xcontext"
@@ -107,6 +108,10 @@ func svalinn(arguments []string) int {
 		f, v                                = pflag.NewFlagSet(applicationName, pflag.ContinueOnError), viper.New()
 		logger, metricsRegistry, codex, err = server.Initialize(applicationName, arguments, f, v, secure.Metrics, db.Metrics, Metrics)
 	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to initialize application: %s\n", err.Error())
+		return 1
+	}
 
 	printVer := f.BoolP("version", "v", false, "displays the version number")
 	if err := f.Parse(arguments); err != nil {
@@ -119,102 +124,195 @@ func svalinn(arguments []string) int {
 		return 0
 	}
 
+	logging.Info(logger).Log(logging.MessageKey(), "Successfully loaded config file", "configurationFile", v.ConfigFileUsed())
+	config := parseConfig(v, codex.Server)
+
+	app, dbConn, serverHealth, requestParser, batchInserter, stopUpdateBlackList, err := createTasks(logger, config, metricsRegistry, v)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to initialize viper: %s\n", err.Error())
+		fmt.Fprintf(os.Stderr, "Unable to create task (refer to the log file): %s\n", err)
 		return 1
 	}
-	logging.Info(logger).Log(logging.MessageKey(), "Successfully loaded config file", "configurationFile", v.ConfigFileUsed())
 
-	serverHealth := health.New()
-	serverHealth.Logger = healthlogger.NewHealthLogger(logger)
-
-	config := new(SvalinnConfig)
-	v.Unmarshal(config)
-
-	if config.ParseQueueSize < defaultMinParseQueueSize {
-		config.ParseQueueSize = defaultMinParseQueueSize
-	}
-	if config.InsertQueueSize < defaultMinInsertQueueSize {
-		config.InsertQueueSize = defaultMinInsertQueueSize
-	}
-
-	requestQueue := make(chan wrp.Message, config.ParseQueueSize)
-	insertQueue := make(chan db.Record, config.InsertQueueSize)
-
-	dbConn, err := db.CreateDbConnection(config.Db, metricsRegistry, serverHealth)
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to initialize database connection",
-			logging.ErrorKey(), err.Error())
-		fmt.Fprintf(os.Stderr, "Database Initialize Failed: %#v\n", err)
-		return 2
-	}
-
-	cipherOptions, err := cipher.FromViper(v)
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to initialize cipher options",
-			logging.ErrorKey(), err.Error())
-		fmt.Fprintf(os.Stderr, "Cipher Options Initialize Failed: %#v\n", err)
-		return 2
-	}
-
-	encrypter, err := cipherOptions.GetEncrypter(logger)
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to load cipher decrypter",
-			logging.ErrorKey(), err.Error())
-		fmt.Fprintf(os.Stderr, "Cipher decrypter Initialize Failed: %#v\n", err)
-		return 2
-	}
-
-	// Create Metrics
-	measures := NewMeasures(metricsRegistry)
-
-	if config.Webhook.URL == "" {
-		config.Webhook.URL = codex.Server + apiBase + config.Endpoint
-	}
-	secretGetter := NewConstantSecret(config.Webhook.Secret)
-	// if the register interval is 0, don't register
-	if config.Webhook.RegistrationInterval > 0 {
-		registerer := newPeriodicRegisterer(config.Webhook, secretGetter, logger)
-
-		// then continue to register
-		go registerer.registerAtInterval()
-	}
-
+	// Create router
 	customLogInfo := xcontext.Populate(
 		logginghttp.SetLogger(logger,
 			logginghttp.RequestInfo,
 		),
 		gokithttp.PopulateRequestContext,
 	)
-	// TODO: fix bookkeeping, add a decorator to add the bookkeeping requests and logger
-	bookkeeper := bookkeeping.New(bookkeeping.WithResponses(bookkeeping.Code))
 
-	svalinnHandler := alice.New(SetLogger(logger), bookkeeper, customLogInfo)
-	// TODO: add authentication back
-	//svalinnHandler := alice.New(authHandler.Decorate)
+	svalinnHandler := alice.New(SetLogger(logger), customLogInfo)
 	router := mux.NewRouter()
-	// MARK: Actual server logic
 
-	app := &App{
+	// TODO: Fix Caduceus actual register
+	router.Handle(apiBase+config.Endpoint, svalinnHandler.ThenFunc(app.handleWebhook))
+
+	startServer(config, serverHealth, logger, codex, metricsRegistry, router, start, stopUpdateBlackList, requestParser, batchInserter, dbConn)
+	return 0
+}
+
+func startServer(config *SvalinnConfig, serverHealth *health.Health, logger log.Logger, codex *server.WebPA, metricsRegistry xmetrics.Registry, router *mux.Router, start time.Time, stopUpdateBlackList chan struct{}, parser requestParser, inserter batchInserter, dbConn *db.Connection) {
+	err := startHealthEndpoint(config, serverHealth, logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to start health endoint: %s\n", err)
+		return
+	}
+	// MARK: Starting the server
+	_, runnable, done := codex.Prepare(logger, nil, metricsRegistry, router)
+	waitGroup, shutdown, err := concurrent.Execute(runnable)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to start device manager: %s\n", err)
+		return
+	}
+	logging.Info(logger).Log(logging.MessageKey(), fmt.Sprintf("%s is up and running!", applicationName), "elapsedTime", time.Since(start))
+	signals := make(chan os.Signal, 10)
+	signal.Notify(signals)
+	for exit := false; !exit; {
+		select {
+		case s := <-signals:
+			if s != os.Kill && s != os.Interrupt {
+				logging.Info(logger).Log(logging.MessageKey(), "ignoring signal", "signal", s)
+			} else {
+				logging.Error(logger).Log(logging.MessageKey(), "exiting due to signal", "signal", s)
+				exit = true
+			}
+		case <-done:
+			logging.Error(logger).Log(logging.MessageKey(), "one or more servers exited")
+			exit = true
+		}
+	}
+	err = serverHealth.Stop()
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "stopping health endpoint failed",
+			logging.ErrorKey(), err.Error())
+	}
+	close(stopUpdateBlackList)
+	close(shutdown)
+	waitGroup.Wait()
+	close(parser.requestQueue)
+	parser.wg.Wait()
+	close(inserter.insertQueue)
+	inserter.wg.Wait()
+	err = dbConn.Close()
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "closing database threads failed",
+			logging.ErrorKey(), err.Error())
+	}
+}
+
+func parseConfig(v *viper.Viper, defaultServer string) *SvalinnConfig {
+	config := new(SvalinnConfig)
+	v.Unmarshal(config)
+	if config.ParseQueueSize < defaultMinParseQueueSize {
+		config.ParseQueueSize = defaultMinParseQueueSize
+	}
+	if config.InsertQueueSize < defaultMinInsertQueueSize {
+		config.InsertQueueSize = defaultMinInsertQueueSize
+	}
+	if config.Webhook.URL == "" {
+		config.Webhook.URL = defaultServer + apiBase + config.Endpoint
+	}
+	return config
+}
+
+func createTasks(logger log.Logger, config *SvalinnConfig, metricsRegistry provider.Provider, v *viper.Viper) (app *App, dbConn *db.Connection, serverHealth *health.Health, rparser requestParser, bInserter batchInserter, stopUpdateBlackList chan struct{}, err error) {
+	requestQueue := make(chan wrp.Message, config.ParseQueueSize)
+	insertQueue := make(chan db.Record, config.InsertQueueSize)
+
+	serverHealth = health.New()
+	serverHealth.Logger = healthlogger.NewHealthLogger(logger)
+
+	dbConn, err = db.CreateDbConnection(config.Db, metricsRegistry, serverHealth)
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to initialize database connection",
+			logging.ErrorKey(), err.Error())
+		return
+	}
+
+	cipherOptions, err := cipher.FromViper(v)
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to initialize cipher options",
+			logging.ErrorKey(), err.Error())
+		return
+	}
+
+	encrypter, err := cipherOptions.GetEncrypter(logger)
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to load cipher encrypter",
+			logging.ErrorKey(), err.Error())
+		return
+	}
+	inserter := db.CreateRetryInsertService(dbConn, db.WithRetries(config.InsertRetries), db.WithInterval(config.RetryInterval), db.WithMeasures(metricsRegistry))
+
+	measures := NewMeasures(metricsRegistry)
+
+	secretGetter := startRegistration(config, logger)
+
+	app = &App{
 		logger:       logger,
 		requestQueue: requestQueue,
 		secretGetter: secretGetter,
 		measures:     measures,
 	}
 
+	stopUpdateBlackList = make(chan struct{}, 1)
+	blacklistConfig := blacklist.RefresherConfig{
+		Logger:         logger,
+		UpdateInterval: config.BlacklistInterval,
+	}
 	rules, err := createRules(config.RegexRules)
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to load rules",
+			logging.ErrorKey(), err.Error())
+		return
+	}
 
-	// TODO: Fix Caduces acutal register
-	router.Handle(apiBase+config.Endpoint, svalinnHandler.ThenFunc(app.handleWebhook))
+	rparser = requestParser{
+		encrypter:       encrypter,
+		blacklist:       blacklist.NewListRefresher(blacklistConfig, dbConn, stopUpdateBlackList),
+		rules:           rules,
+		payloadMaxSize:  config.PayloadMaxSize,
+		metadataMaxSize: config.MetadataMaxSize,
+		defaultTTL:      config.DefaultTTL,
+		requestQueue:    requestQueue,
+		insertQueue:     insertQueue,
+		maxParseWorkers: config.MaxParseWorkers,
+		measures:        measures,
+		logger:          logger,
+	}
+	err = rparser.validateAndStartParser()
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to validate and start parser",
+			logging.ErrorKey(), err.Error())
+		return
+	}
+	bInserter = batchInserter{
+		inserter:         inserter,
+		logger:           logger,
+		insertQueue:      insertQueue,
+		maxInsertWorkers: config.MaxInsertWorkers,
+		maxBatchSize:     config.MaxBatchSize,
+		maxBatchWaitTime: config.MaxBatchWaitTime,
+		measures:         measures,
+	}
+	err = bInserter.validateAndStartInserter()
+	if err != nil {
+		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to validate and start inserter",
+			logging.ErrorKey(), err.Error())
+		return
+	}
 
-	inserter := db.CreateRetryInsertService(dbConn, db.WithRetries(config.InsertRetries), db.WithInterval(config.RetryInterval), db.WithMeasures(metricsRegistry))
+	return
+}
 
+func createWorkers(logger log.Logger, config *SvalinnConfig, encrypter cipher.Encrypt, dbConn *db.Connection, rules []rule, measures *Measures, inserter db.RetryInsertService) (chan struct{}, requestParser, batchInserter) {
+	requestQueue := make(chan wrp.Message, config.ParseQueueSize)
+	insertQueue := make(chan db.Record, config.InsertQueueSize)
 	stopUpdateBlackList := make(chan struct{}, 1)
 	blacklistConfig := blacklist.RefresherConfig{
 		Logger:         logger,
 		UpdateInterval: config.BlacklistInterval,
 	}
-
 	requestParser := requestParser{
 		encrypter:       encrypter,
 		blacklist:       blacklist.NewListRefresher(blacklistConfig, dbConn, stopUpdateBlackList),
@@ -237,21 +335,10 @@ func svalinn(arguments []string) int {
 		maxBatchWaitTime: config.MaxBatchWaitTime,
 		measures:         measures,
 	}
-	err = requestParser.validateAndStartParser()
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to validate and start parser",
-			logging.ErrorKey(), err.Error())
-		fmt.Fprintf(os.Stderr, "Validating and starting parser failed: %#v\n", err)
-		return 2
-	}
-	err = batchInserter.validateAndStartInserter()
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "Failed to validate and start inserter",
-			logging.ErrorKey(), err.Error())
-		fmt.Fprintf(os.Stderr, "Validating and starting inserter failed: %#v\n", err)
-		return 2
-	}
+	return stopUpdateBlackList, requestParser, batchInserter
+}
 
+func startHealthEndpoint(config *SvalinnConfig, serverHealth *health.Health, logger log.Logger) (err error) {
 	if config.Health.Endpoint != "" && config.Health.Port != "" {
 		err = serverHealth.Start()
 		if err != nil {
@@ -263,52 +350,19 @@ func svalinn(arguments []string) int {
 			olog.Fatal(http.ListenAndServe(config.Health.Port, nil))
 		}()
 	}
+	return err
+}
 
-	// MARK: Starting the server
-	_, runnable, done := codex.Prepare(logger, nil, metricsRegistry, router)
+func startRegistration(config *SvalinnConfig, logger log.Logger) *constantSecret {
+	secretGetter := NewConstantSecret(config.Webhook.Secret)
+	// if the register interval is 0, don't register
+	if config.Webhook.RegistrationInterval > 0 {
+		registerer := newPeriodicRegisterer(config.Webhook, secretGetter, logger)
 
-	waitGroup, shutdown, err := concurrent.Execute(runnable)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to start device manager: %s\n", err)
-		return 1
+		// then continue to register
+		go registerer.registerAtInterval()
 	}
-
-	logging.Info(logger).Log(logging.MessageKey(), fmt.Sprintf("%s is up and running!", applicationName), "elapsedTime", time.Since(start))
-	signals := make(chan os.Signal, 10)
-	signal.Notify(signals)
-	for exit := false; !exit; {
-		select {
-		case s := <-signals:
-			if s != os.Kill && s != os.Interrupt {
-				logging.Info(logger).Log(logging.MessageKey(), "ignoring signal", "signal", s)
-			} else {
-				logging.Error(logger).Log(logging.MessageKey(), "exiting due to signal", "signal", s)
-				exit = true
-			}
-		case <-done:
-			logging.Error(logger).Log(logging.MessageKey(), "one or more servers exited")
-			exit = true
-		}
-	}
-
-	err = serverHealth.Stop()
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "stopping health endpoint failed",
-			logging.ErrorKey(), err.Error())
-	}
-	close(stopUpdateBlackList)
-	close(shutdown)
-	waitGroup.Wait()
-	close(requestQueue)
-	requestParser.wg.Wait()
-	close(insertQueue)
-	batchInserter.wg.Wait()
-	err = dbConn.Close()
-	if err != nil {
-		logging.Error(logger, emperror.Context(err)...).Log(logging.MessageKey(), "closing database threads failed",
-			logging.ErrorKey(), err.Error())
-	}
-	return 0
+	return secretGetter
 }
 
 func main() {
