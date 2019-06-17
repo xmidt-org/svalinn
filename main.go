09fca8db9c6137ec7bcad5a437bffd49c016e18a
Concourse CI <git_concourse_ci@comcast.com>
2019-06-17 16:48:48 +0000

    [skip ci] bump version from 0.9.0 to 0.9.1

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ import (
 const (
 	applicationName, apiBase = "svalinn", "/api/v1"
 	DEFAULT_KEY_ID           = "current"
-	applicationVersion       = "0.9.0"
+	applicationVersion       = "0.9.1"
 )
 
 type SvalinnConfig struct {
