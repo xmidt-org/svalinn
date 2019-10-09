1665c37fc7ec10cb586ccdb3757b1535e3ca6cd4
Jack Murdock <kcajmagic@gmail.com>
2019-10-09 11:34:15 -0700

    fix docker-compose standup error

--- it.sh
+++ it.sh
@@ -19,7 +19,7 @@ function deploy {
     echo "Deploying Cluster"
     git clone https://github.com/xmidt-org/codex-deploy.git 2> /dev/null || true
     pushd codex-deploy/deploy/docker-compose
-    SVALINN_VERSION=$SVALINN_VERSION docker-compose up -d db db-init svalinn
+    SVALINN_VERSION=$SVALINN_VERSION docker-compose up -d yb-master yb-tserver db-init svalinn
     check $?
     popd
     printf "\n"
